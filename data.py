c9f8156470d83b57e5676a68ce7877c95c82350c
Csaba <todorcs@gmail.com>
2017-05-25 17:19:23 +0300

    Created Uses Data (Mock)

---
+++ data.py
@@ -0,0 +1,37 @@
+def Users():
+    users = [
+        {
+            'id': 0,
+            'name': 'Csaba',
+            'username': 'csabi',
+            'bio': 'Lorem Ipsum Dolor',
+            'reg_date': '25/05/2017',
+            'birth_date': '06/06/1986'
+        },
+        {
+            'id': 1,
+            'name': 'Kimberly',
+            'username': 'kim',
+            'bio': 'Lorem Ipsum Dolor Kim',
+            'reg_date': '25/06/2017',
+            'birth_date': '09/09/1995'
+        },
+        {
+            'id': 2,
+            'name': 'Johny',
+            'username': 'john',
+            'bio': 'Lorem Ipsum Dolor John',
+            'reg_date': '24/05/2017',
+            'birth_date': '01/11/1980'
+        },
+        {
+            'id': 3,
+            'name': 'Patricia',
+            'username': 'pat',
+            'bio': 'Lorem Ipsum Dolor Patricia',
+            'reg_date': '21/05/2017',
+            'birth_date': '08/12/1989'
+        }
+    ]
+
+    return users
