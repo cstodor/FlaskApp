f49c84f0bd688c421ff8753b89fa5830e95ea0d3
Csaba <todorcs@gmail.com>
2017-05-25 17:20:26 +0300

    Created Users Route and users-list template

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from data import Users # users data
 # placeholder for the current module (app.py)
 app = Flask(__name__)
 
+# initialize Users variablke with Users(function)
+Users = Users()
+
 # app routes
 # root
 @app.route('/')
@@ -14,6 +17,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html') # return about template
+
+# users
+@app.route('/users')
+def users():
+    return render_template('users-list.html', users = Users) # return users-list template and pass in the data (Users)
 
 # executed app
 if __name__ == '__main__':
