ebc2b8fc52ceb4b278c9583b5719fbdec4a2e9d1
Csaba <todorcs@gmail.com>
2017-05-25 17:18:15 +0300

    Created About Template and Route

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html') # return index template
+
+# about
+@app.route('/about')
+def about():
+    return render_template('about.html') # return about template
+
 # executed app
 if __name__ == '__main__':
     app.run(debug=True) # debug=True updates View if detects changes
